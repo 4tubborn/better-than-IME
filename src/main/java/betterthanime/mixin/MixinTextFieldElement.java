800c34f71e63157ef0eea013ee1f2a31ce84bf81
4tubborn <highocean106@gmail.com>
2026-03-13 17:30:08 +0800

    remove unused classes

--- src/main/java/betterthanime/mixin/MixinTextFieldElement.java
+++ src/main/java/betterthanime/mixin/MixinTextFieldElement.java
@@ -1,10 +1,8 @@
 package betterthanime.mixin;
 
-import betterthanime.util.IMEStateManager;
 import betterthanime.util.IMEUtil;
 import net.minecraft.client.gui.TextFieldElement;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
